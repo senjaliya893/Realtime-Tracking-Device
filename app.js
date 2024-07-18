017a78aed8f713ca6aeea48a3cacfe99df7280f9
ViVEK SENJALiYA <154584056+senjaliya893@users.noreply.github.com>
2024-07-18 11:47:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+const http = require("http");
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection", function (socket) {
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", {id: socket.id, ...data});
+    });
+    
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id);
+    });
+
+});
+
+app.get("/", function (req,res) {
+    res.render("index");
+})
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
